c074d3835f09d3bd2e9d02d90904dc2b12d863a0
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-06-27 23:43:48 +0530

    com

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,8 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Sidebar from '../components/Sidebar';
-import RightSidebar from '../components/RightSidebar';
+
 import Navbar from '../components/Navbar';
 import BASE_URL from '../config';
 import Footer from '../components/Footer';
@@ -27,17 +26,6 @@ export default function DashboardLayout() {
     <>
     <div className="flex min-h-screen">
       {/* Sidebar (hidden on mobile, toggleable) */}
-      <div className={`fixed z-40 inset-y-0 left-0 w-64 transform transition-transform duration-200 bg-white shadow-lg md:relative md:translate-x-0 ${showSidebar ? 'translate-x-0' : '-translate-x-full'} md:block`}>
-        <Sidebar />
-      </div>
-
-      {/* Overlay for mobile sidebar */}
-      {showSidebar && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-30 z-30 md:hidden"
-          onClick={toggleSidebar}
-        ></div>
-      )}
 
       <div className="flex-1 flex flex-col">
         <Navbar toggleSidebar={toggleSidebar} />
@@ -47,9 +35,6 @@ export default function DashboardLayout() {
           </main>
 
           {/* Right Sidebar (hidden on small screens) */}
-          <div className="hidden lg:block w-64">
-            <RightSidebar />
-          </div>
         </div>
       </div>
       <FloatingButtons buttonType="bars" buttonsList={buttonsList} direction="up" />
